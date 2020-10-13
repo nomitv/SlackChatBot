86ce7e8a8af57a18eb37eb12bb36182a118e059a
Nomit Verma <nomitverma1@gmail.com>
2020-10-13 12:46:49 +0530

    Add styles to ScheduleMessage

--- screens/ScheduleMessage.js
+++ screens/ScheduleMessage.js
@@ -86,3 +86,57 @@ export default class ScheduleMessage extends React.Component {
                 )
             }
         }
+
+        const pickerStyle = {
+            inputIOS: {
+                color: 'white',
+                paddingHorizontal: 10,
+                backgroundColor: 'red',
+                borderRadius: 5,
+            },
+            placeholder: {
+                color: 'black',
+              },
+            inputAndroid: {
+                color: 'black',
+                paddingHorizontal: 10,
+                backgroundColor: 'white',
+                borderRadius: 5,
+            },
+        };
+        
+        const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+                backgroundColor: '#003f5c',
+                alignItems: 'center',
+                justifyContent: 'flex-start',
+              },
+              logo:{
+                fontWeight:"bold",
+                fontSize:40,
+                color:"#fb5b5a",
+                marginTop: 30
+              },
+              loginText:{
+                color:"white",
+                fontSize:15
+              },
+              loginBtn:{
+                width:200,
+                backgroundColor:"#fb5b5a",
+                borderRadius:25,
+                height:50,
+                alignItems:"center",
+                justifyContent:"center",
+                marginTop:40,
+                marginBottom:10,
+                marginHorizontal: 50
+              },
+              inputAndroid: {
+                color: 'white',
+                paddingHorizontal: 10,
+                backgroundColor: 'red',
+                borderRadius: 5,
+            },
+          });
